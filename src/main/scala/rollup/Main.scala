9c3f8a4f4a6c80c5f9d2ed20b40916ade6e34f00
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2020-04-17 21:47:30 +0200

    Make rollup tail recursive

--- src/main/scala/rollup/Main.scala
+++ src/main/scala/rollup/Main.scala
@@ -25,20 +25,22 @@ object Main {
 
     val rdd = df.rdd
 
-    var groupingList = List(0, 1, 3)
+    var groupingList = List(4, 0, 1, 3)
     val rollup = new RollupOperator
 
     // val res = rollup.rollup_naive(rdd, groupingList, 8, "AVG")
-    val res = rollup.rollup(rdd, groupingList, 8, "SUM")
+    val res = rollup.rollup(rdd, groupingList, 8, "AVG")
     // res.foreach(x => println(x))
 
     // use the following code to evaluate the correctness of your results
-    val correctRes = df.rollup("lo_orderkey", "lo_linenumber", "lo_partkey")
-      .agg(sum("lo_quantity")).rdd
+    val correctRes = df.rollup("lo_suppkey", "lo_orderkey", "lo_linenumber", "lo_partkey")
+      .agg(avg("lo_quantity")).rdd
       .map(row => (row.toSeq.toList.dropRight(1).filter(x => x != null), row(row.size - 1)))
     // correctRes.foreach(x => println(x))
 
-    res.subtract(correctRes.asInstanceOf[RDD[(scala.List[Any], Double)]]).collect.foreach(println)
-    correctRes.asInstanceOf[RDD[(scala.List[Any], Double)]].subtract(res).collect.foreach(println)
+    val excess = res.subtract(correctRes.asInstanceOf[RDD[(scala.List[Any], Double)]]).collect
+    excess.foreach(println)
+    val less = correctRes.asInstanceOf[RDD[(scala.List[Any], Double)]].subtract(res).collect
+    less.foreach(println)
   }
 }
--- src/main/scala/rollup/RollupOperator.scala
+++ src/main/scala/rollup/RollupOperator.scala
@@ -3,73 +3,30 @@ package rollup
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 
-sealed trait Dataset
-
-final case class First(f: RDD[Row]) extends Dataset
-
-final case class Other(f: RDD[(List[Any], Double, Int)]) extends Dataset
-
 class RollupOperator() extends Serializable {
+
+  def rollup_indices(dataset: RDD[Row], groupingAttributeIndexes: List[Int], aggAttributeIndex: Int, agg: String): RDD[(List[Any], Double, Int)] = {
+
+    val grouped: RDD[(List[Any], Iterable[(Double, Int)])] =
+      dataset
+        .groupBy(row => groupingAttributeIndexes.map(i => row(i))) // Group by the needed indices
+        .map(t => (t._1, t._2.map(s => s(aggAttributeIndex)))) // Get the attribute to aggregate
+        .map(t => (t._1, t._2.map(s => (castField(s), 1)))) // Transform the attribute to Double and couple with 1
+
+    val aggregated: RDD[(List[Any], Double, Int)] = computeAggregate(agg, grouped)
+
+    groupingAttributeIndexes match {
+      case Nil => aggregated
+      case _ => rollup_indices(groupingAttributeIndexes.indices.dropRight(1).toList, aggregated, aggregated, agg)
+    }
+
+  }
 
   def rollup(dataset: RDD[Row], groupingAttributeIndexes: List[Int], aggAttributeIndex: Int, agg: String): RDD[(List[Any], Double)] = {
 
-    def rollup_indices(indices: List[Int], aggregated_dataset: Dataset): RDD[(List[Any], Double, Int)] = {
+    rollup_indices(dataset, groupingAttributeIndexes, aggAttributeIndex, agg)
+      .map(t => (t._1, t._2))
 
-      val grouped: RDD[(List[Any], Iterable[(Double, Int)])] = aggregated_dataset match {
-        case First(rows_rdd) => rows_rdd
-          .groupBy(row => indices.map(i => row(i)))
-          .map(t => (t._1, t._2.map(r => r.get(aggAttributeIndex))))
-          .map(t => (t._1, t._2.map(castField).zip(Iterable(t._2.size))))
-
-        case Other(aggregated_rdd) => aggregated_rdd
-          .groupBy(t => indices.map(i => t._1(i)))
-          .map(t => (t._1, t._2.map(_._2).zip(t._2.map(_._3))))
-      }
-
-      val aggregated: RDD[(List[Any], Double, Int)] = agg match {
-        case "SUM" => grouped.map(t => (t._1, t._2.map(_._1).sum, t._2.size))
-        case "MIN" => grouped.map(t => (t._1, t._2.map(_._1).min, t._2.size))
-        case "MAX" => grouped.map(t => (t._1, t._2.map(_._1).max, t._2.size))
-        case "COUNT" => grouped.map(t => (t._1, t._2.map(_._2).sum, t._2.size))
-        case "AVG" => grouped.map(t => (t._1, t._2.map(t_i => t_i._1 * t_i._2).sum / t._2.map(_._2).sum, t._2.size))
-      }
-
-      aggregated
-    }
-
-    var total = rollup_indices(groupingAttributeIndexes, First(dataset))
-    var previous = total
-
-    for (i <- groupingAttributeIndexes.indices.reverse) {
-      val indices = (0 until i).toList
-
-      val new_rdd = rollup_indices(indices, Other(previous))
-
-      total = total.union(new_rdd)
-      previous = new_rdd
-
-    }
-
-    total.map(t => (t._1, t._2))
-  }
-
-  /*
- * This method gets as input one dataset, the indexes of the grouping attributes of the rollup (ROLLUP clause)
- * the index of the attribute on which the aggregation is performed
- * and the aggregate function (it has to be one of "COUNT", "SUM", "MIN", "MAX", "AVG")
- * and returns an RDD with the result in the form of <key = List[Any], value = Double> pairs.
- * The key is used to uniquely identify a group that corresponds to a certain combination of attribute values.
- * You are free to do that following your own naming convention.
- * The value is the aggregation result.
- * You are not allowed to change the definition of this function or the names of the aggregate functions.
- * */
-
-  private def castField: Any => Double = {
-    case f: Int => f.toDouble
-    case f: Double => f
-    case f: Long => f.toDouble
-    case f: Float => f.toDouble
-    case _ => throw new IllegalArgumentException("The field is not numeric.")
   }
 
   def rollup_naive(dataset: RDD[Row], groupingAttributeIndexes: List[Int], aggAttributeIndex: Int, agg: String): RDD[(List[Any], Double)] = {
@@ -106,5 +63,61 @@ class RollupOperator() extends Serializable {
 
     rollups
 
+  }
+
+  private def castField: Any => Double = {
+    case f: Int => f.toDouble
+    case f: Double => f
+    case f: Long => f.toDouble
+    case f: Float => f.toDouble
+    case _ => throw new IllegalArgumentException("The field is not numeric.")
+  }
+
+  /*
+ * This method gets as input one dataset, the indexes of the grouping attributes of the rollup (ROLLUP clause)
+ * the index of the attribute on which the aggregation is performed
+ * and the aggregate function (it has to be one of "COUNT", "SUM", "MIN", "MAX", "AVG")
+ * and returns an RDD with the result in the form of <key = List[Any], value = Double> pairs.
+ * The key is used to uniquely identify a group that corresponds to a certain combination of attribute values.
+ * You are free to do that following your own naming convention.
+ * The value is the aggregation result.
+ * You are not allowed to change the definition of this function or the names of the aggregate functions.
+ * */
+
+  @scala.annotation.tailrec
+  private def rollup_indices(indices: List[Int], last_aggregated: RDD[(List[Any], Double, Int)], dataset: RDD[(List[Any], Double, Int)], agg: String): RDD[(List[Any], Double, Int)] = {
+
+    val grouped: RDD[(List[Any], Iterable[(Double, Int)])] =
+      last_aggregated
+        .groupBy(t => indices.map(i => t._1(i))) // Group by the needed indices
+        .map(t => (t._1, t._2.map(s => (s._2, s._3)))) // Save the couple of aggregated elements
+
+    val aggregated: RDD[(List[Any], Double, Int)] = computeAggregate(agg, grouped)
+
+    indices match {
+      case Nil => aggregated.union(dataset)
+      case _ => rollup_indices(indices.dropRight(1), aggregated, aggregated.union(dataset), agg)
+    }
+
+  }
+
+  private def computeAggregate(agg: String, grouped: RDD[(List[Any], Iterable[(Double, Int)])]): RDD[(List[Any], Double, Int)] = {
+
+    def avg(t: (List[Any], Iterable[(Double, Int)])): (List[Any], Double, Int) = {
+
+      val weightedSum = t._2.map(s => s._1 * s._2).sum
+      val denominator = t._2.map(_._2).sum
+      val average = weightedSum / denominator
+
+      (t._1, average, t._2.size)
+    }
+
+    agg match {
+      case "SUM" => grouped.map(t => (t._1, t._2.map(_._1).sum, t._2.size))
+      case "MIN" => grouped.map(t => (t._1, t._2.map(_._1).min, t._2.size))
+      case "MAX" => grouped.map(t => (t._1, t._2.map(_._1).max, t._2.size))
+      case "COUNT" => grouped.map(t => (t._1, t._2.map(_._2).sum, t._2.size))
+      case "AVG" => grouped.map(avg)
+    }
   }
 }
