d67c6543b7299d00b59a99c0549ad1e1ecc6f079
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2020-04-25 17:18:27 +0200

    Improve getBucket

--- src/main/scala/thetajoin/Main.scala
+++ src/main/scala/thetajoin/Main.scala
@@ -1,11 +1,9 @@
 package thetajoin
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.{SQLContext, Row}
-
-
 import java.io._
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext, SparkSession}
 
 object Main {
   def main(args: Array[String]) {
@@ -15,32 +13,40 @@ object Main {
       .master("local[*]")
       .getOrCreate()
 
-    val attrIndex1 = 0
-    val attrIndex2 = 0
+    val attrIndex1 = 1
+    val attrIndex2 = 1
 
-//    val rdd1 = loadRDD(spark.sqlContext, "/dat1_4.csv")
-//    val rdd2 = loadRDD(spark.sqlContext, "/dat2_4.csv")
-//
-//    val thetaJoin = new ThetaJoin(4)
-//    val res = thetaJoin.ineq_join(rdd1, rdd2, attrIndex1, attrIndex2, "<")
-//    res.foreach(x => println(x))
+    //    val rdd1 = loadRDD(spark.sqlContext, "/dat1_4.csv")
+    //    val rdd2 = loadRDD(spark.sqlContext, "/dat2_4.csv")
+    //
+    //    val thetaJoin = new ThetaJoin(4)
+    //    val res = thetaJoin.ineq_join(rdd1, rdd2, attrIndex1, attrIndex2, "<")
+    //    res.foreach(x => println(x))
 
 
-    val rdd1 = loadRDD(spark.sqlContext, "/dat1_6.csv")
-    val rdd2 = loadRDD(spark.sqlContext, "/dat2_9.csv")
+    val rdd1 = loadRDD(spark.sqlContext, "/taxA4K.csv")
+    val rdd2 = loadRDD(spark.sqlContext, "/taxB4K.csv")
 
-    val thetaJoin = new ThetaJoin(6)
+    // val rdd1 = loadLineorderRdd(spark.sqlContext, "/skewed_data.csv")
+    // val rdd2 = loadLineorderRdd(spark.sqlContext, "/skewed_data.csv")
+
+    val thetaJoin = new ThetaJoin(64)
     val res = thetaJoin.ineq_join(rdd1, rdd2, attrIndex1, attrIndex2, "<")
-    println(res.count)
+    // println(res.count)
 
+    val start = System.currentTimeMillis()
+    res.collect()
+    val end = System.currentTimeMillis()
+
+    println(s"Took ${(end - start) / 1000}s")
 
     println("----------------------")
     // use the cartesian product to verify correctness of your result
     val cartesianRes = rdd1.cartesian(rdd2)
-                         .filter(x => x._1(attrIndex1).asInstanceOf[Int] < x._2(attrIndex2).asInstanceOf[Int])
-                         .map(x => (x._1(attrIndex1).asInstanceOf[Int], x._2(attrIndex2).asInstanceOf[Int]))
-    cartesianRes.foreach(x => println(x))
-    assert(res.sortBy(x => (x._1, x._2)).collect().toList.equals(cartesianRes.sortBy(x => (x._1, x._2)).collect().toList) )
+      .filter(x => x._1(attrIndex1).asInstanceOf[Int] < x._2(attrIndex2).asInstanceOf[Int])
+      .map(x => (x._1(attrIndex1).asInstanceOf[Int], x._2(attrIndex2).asInstanceOf[Int]))
+    // cartesianRes.foreach(x => println(x))
+    // assert(res.sortBy(x => (x._1, x._2)).collect().toList.equals(cartesianRes.sortBy(x => (x._1, x._2)).collect().toList) )
   }
 
   def loadRDD(sqlContext: SQLContext, file: String): RDD[Row] = {
@@ -51,6 +57,20 @@ object Main {
       .option("header", "false")
       .option("inferSchema", "true")
       .option("delimiter", ",")
+      .load(input)
+      // .limit(100)
+      .rdd
+  }
+
+  def loadLineorderRdd(sqlContext: SQLContext, file: String): RDD[Row] = {
+    val input = new File(getClass.getResource(file).getFile).getPath
+
+    sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .option("delimiter", "|")
       .load(input).rdd
   }
+
 }
--- src/main/scala/thetajoin/ThetaJoin.scala
+++ src/main/scala/thetajoin/ThetaJoin.scala
@@ -5,8 +5,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.slf4j.LoggerFactory
 
-import scala.collection.immutable.TreeMap
-
 class ThetaJoin(partitions: Int) extends java.io.Serializable {
   val logger = LoggerFactory.getLogger("ThetaJoin")
 
@@ -20,99 +18,132 @@ class ThetaJoin(partitions: Int) extends java.io.Serializable {
    */
   def ineq_join(dat1: RDD[Row], dat2: RDD[Row], attrIndex1: Int, attrIndex2: Int, condition: String): RDD[(Int, Int)] = {
 
-    val R = dat1.map(_(attrIndex1).asInstanceOf[Int])
-    val S = dat2.map(_(attrIndex2).asInstanceOf[Int])
+    val R = dat1.map(_ (attrIndex1).asInstanceOf[Int])
+    val S = dat2.map(_ (attrIndex2).asInstanceOf[Int])
 
     val cardR = dat1.count()
     val cardS = dat2.count()
     val cR = (cardR / scala.math.sqrt(cardS * cardR / partitions)).toInt
     val cS = (cardS / scala.math.sqrt(cardS * cardR / partitions)).toInt
 
-    val quantilesR = R.takeSample(false, cR - 1).sorted
-    val quantilesS = S.takeSample(false, cS - 1).sorted
+    val quantilesR = R.takeSample(withReplacement = false, cR - 1).sorted
+    val quantilesS = S.takeSample(withReplacement = false, cS - 1).sorted
 
-    val regionsR: RDD[(Int, IndexedSeq[Int])] = R
-      .map(r => (r, getBucket(r, quantilesR)))
-      //.map(t => (t._1, getRowOrCols(t._2, cR, R)))
-      .map(t => (t._1, getRegions(t._2, cS, "R")))
+    val sortedR = R.collect.sorted
+    val sortedS = S.collect.sorted
+    val rQuantilesIndices = 0 to sortedR.length by (sortedR.length / cR)
 
-    val mapR: RDD[(Int, (Int, String))] = regionsR
-      .flatMap { case (attr, regions) => regions.map((attr, _)) }
-      .map(t => (t._2, (t._1, "R")))
+    println(s"R quantiles: ${quantilesR.mkString(", ")}")
+    println(s"S quantiles: ${quantilesS.mkString(", ")}")
 
-    val regionsS: RDD[(Int, IndexedSeq[Int])] = S
-      .map(r => (r, getBucket(r, quantilesS)))
-      //.map(t => (t._1, getRowOrCols(t._2, cR, R)))
-      .map(t => (t._1, getRegions(t._2, cS, "S")))
+    val regionsR: RDD[(Int, IndexedSeq[Int])] =
+      R
+        .map(r => (r, getBucket(r, quantilesR)))
+        //.map(t => (t._1, getRowOrCols(t._2, cR, R)))
+        .map(t => (t._1, getRegions(t._2, cS, "R")))
 
-    val mapS: RDD[(Int, (Int, String))] = regionsS
-      .flatMap { case (attr, regions) => regions.map((attr, _)) }
-      .map(t => (t._2, (t._1, "S")))
+    val mapR: RDD[(Int, (Int, String))] =
+      regionsR
+        .flatMap { case (attr, regions) => regions.map((attr, _)) }
+        .map(t => (t._2, (t._1, "R")))
 
-//    val mapR: RDD[(Int, Int)] = regionsR
-//      .flatMap { case (attr, regions) => regions.map((attr, _)) }
-//
-//    val mapS: RDD[(Int, Int)] = regionsS
-//      .flatMap { case (attr, regions) => regions.map((attr, _)) }
+    val regionsS: RDD[(Int, IndexedSeq[Int])] =
+      S
+        .map(r => (r, getBucket(r, quantilesS)))
+        //.map(t => (t._1, getRowOrCols(t._2, cR, R)))
+        .map(t => (t._1, getRegions(t._2, cS, "S")))
 
-    val M: RDD[(Int, (Int, String))] = mapR.union(mapS)
+    val mapS: RDD[(Int, (Int, String))] =
+      regionsS
+        .flatMap { case (attr, regions) => regions.map((attr, _)) }
+        .map(t => (t._2, (t._1, "S")))
+
+    //    val mapR: RDD[(Int, Int)] = regionsR
+    //      .flatMap { case (attr, regions) => regions.map((attr, _)) }
+    //
+    //    val mapS: RDD[(Int, Int)] = regionsS
+    //      .flatMap { case (attr, regions) => regions.map((attr, _)) }
+
+    val M: RDD[(Int, (Int, String))] =
+      mapR
+        .union(mapS)
         .partitionBy(new BucketPartitioner(partitions))
 
-//    println(M.getNumPartitions)
-//    var p = M.glom().collect()
-//    p.foreach(println)
+    //    println(M.getNumPartitions)
+    //    var p = M.glom().collect()
+    //    p.foreach(println)
 
     val joined: RDD[(Int, Int)] = M.mapPartitions(t => reducePartition(t, condition))
 
     joined
   }
 
-  def getBucket(attr: Int, quantiles: Array[Int]): Int = {
+  private def getBucket(attr: Int, quantiles: Array[Int]): Int = {
     var bucket = 0
+    var buckets: List[Int] = Nil
+    var prev_v = 0
     for ((v, i) <- quantiles.zipWithIndex) {
       if (attr >= v) {
-        bucket = i+1
-        return bucket
+        bucket = i + 1
+        // return bucket
+        if (v == prev_v) {
+          buckets :+= bucket
+        }
+        else if (buckets == Nil) {
+          return bucket
+        } else {
+          val rnd = new scala.util.Random
+          return buckets(rnd.nextInt(buckets.size))
+        }
+
       }
+      prev_v = v
     }
     bucket
   }
 
-  def getRowOrCols(bucket: Int, c: Int, dim: Long): Int = {
-    val buckL = dim/c
-    val start = bucket*buckL.toInt
-    val end   = bucket match {
-      case `c` => dim.toInt
-      case _ => (bucket+1)*buckL.toInt
-    }
-    val rnd = new scala.util.Random
-    start + rnd.nextInt(end - start)
-  }
-
-  def getRegions(bucket: Int, cS: Int, relation: String): IndexedSeq[Int] = {
+  private def getRegions(bucket: Int, cS: Int, relation: String): IndexedSeq[Int] = {
     relation match {
-      case "R" => bucket*cS until (bucket*cS + cS)
+      case "R" => bucket * cS until (bucket * cS + cS)
       case "S" => bucket until partitions by cS
     }
+  }
+
+  private def reducePartition(tuples: Iterator[(Int, (Int, String))], condition: String): Iterator[(Int, Int)] = {
+
+    val tupTot = tuples.toList.partition(t => t._2._2 == "R")
+
+    println(s"Input tuples in reducer: ${tupTot._1.size + tupTot._2.size}")
+
+    val tupR = tupTot._1.map(t => t._2._1)
+    val tupS = tupTot._2.map(t => t._2._1)
+
+    val cross = tupR.flatMap(x => tupS.map(y => (x, y)))
+
+    val result = condition match {
+      case "<" => cross.filter(c => c._1 < c._2)
+      case ">" => cross.filter(c => c._1 > c._2)
+      case _ => throw new IllegalArgumentException("Wrong condition selected")
+    }
+
+    println(s"Output tuples in reducer: ${result.size}")
+
+    result.toIterator
+
   }
 
   class BucketPartitioner(override val numPartitions: Int) extends Partitioner {
     def getPartition(key: Any): Int = key.asInstanceOf[Int]
   }
 
-
-  def reducePartition(tuples: Iterator[(Int, (Int, String))], condition:String): Iterator[(Int, Int)] = {
-    val tupTot = tuples.toList.partition(t => t._2._2 == "R")
-    val tupR = tupTot._1.map(t => t._2._1)
-    val tupS = tupTot._2.map(t => t._2._1)
-
-    val cross = tupR.flatMap(x => tupS.map(y => (x, y)))
-
-    condition match {
-      case "<" => cross.filter(c => c._1 < c._2).toIterator
-      case ">" => cross.filter(c => c._1 > c._2).toIterator
-      case _ => throw new IllegalArgumentException("Wrong condition selected")
+  private def getRowOrCols(bucket: Int, c: Int, dim: Long): Int = {
+    val buckL = dim / c
+    val start = bucket * buckL.toInt
+    val end = bucket match {
+      case `c` => dim.toInt
+      case _ => (bucket + 1) * buckL.toInt
     }
-
+    val rnd = new scala.util.Random
+    start + rnd.nextInt(end - start)
   }
 }
